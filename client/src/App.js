7dcba1426cb4b870a6e9475ff82cb7761aa21706
Ilya1721 <ilyavusnya@gmail.com>
2020-10-21 19:13:09 +0300

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import Navbar from "./components/Navbar";
 import Welcome from "./components/Welcome";
 import Footer from "./components/Footer";
@@ -9,23 +12,25 @@ import Doctors from "./components/Doctors";
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Navbar />
-        <main className="py-4">
-          <Switch>
-            <Route exact path="/">
-              <Welcome />
-            </Route>
-            <Route exact path="/clinics">
-              <Clinics />
-            </Route>
-            <Route exact path="/doctors">
-              <Doctors />
-            </Route>
-          </Switch>
-        </main>
-        <Footer />
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+          <main className="py-4">
+            <Switch>
+              <Route exact path="/">
+                <Welcome />
+              </Route>
+              <Route exact path="/clinics">
+                <Clinics />
+              </Route>
+              <Route exact path="/doctors">
+                <Doctors />
+              </Route>
+            </Switch>
+          </main>
+          <Footer />
+        </Router>
+      </Provider>
     );
   }
 }
